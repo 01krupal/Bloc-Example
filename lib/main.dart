2f3cd3951b586fe2060330d41f6b742788763f5e
krupal <01krupal@gmail.com>
2024-03-15 12:15:32 +0530

    counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_example/bloc/counter/counter_bloc.dart';
 import 'package:bloc_example/ui/counter_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,12 +9,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CounterScreen(),debugShowCheckedModeBanner: false,
-    );
+    return BlocProvider(
+        create: (_) => CounterBloc(),
+        child: const MaterialApp(
+          home: CounterScreen(),
+          debugShowCheckedModeBanner: false,
+        ));
   }
 }
